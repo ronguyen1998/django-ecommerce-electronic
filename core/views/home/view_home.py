bd641db186813a4b9c9f569b9286147f8ddda92f
ronguyen1998 <ronguyen1998@gmail.com>
2022-04-19 18:03:42 +0700

    update code 19042022

--- core/views/home/view_home.py
+++ core/views/home/view_home.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+
+from core.models import Product, Category
+
 # Create your views here.
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')
+        list_product = Product.objects.all()
+        
+
+        context = {
+            "list_product":list_product,
+            
+        }
+        return render(request, 'home/index.html', context)
+
     def post(self, request):
         pass
